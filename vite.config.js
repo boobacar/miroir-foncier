5277e82e2a7906d685a3df3bb5a9437ea59b4b8c
boobacar <boubsfal@gmail.com>
2025-05-18 01:39:26 +0000

    fixing base url again

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/miroir-foncier/',
+  base: '/',
   plugins: [tailwindcss(), react()],
   // base: process.env.NODE_ENV === 'production' ? '/miroir-foncier/' : '/',
   
